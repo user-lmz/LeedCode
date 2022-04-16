b9c17b968032bb9eeb2e285c0f2ce340a97850a7
user-lmz <mingzeluo888@gmail.com>
2022-04-16 13:19:12 +0800

    se

--- src/main/java/Solution85.java
+++ src/main/java/Solution85.java
@@ -4,26 +4,24 @@ import java.util.LinkedList;
 public class Solution85 {
     public static int maximalRectangle(char[][] matrix) {
         int m = matrix.length;
-        if (m == 0) {
+        if (m == 0)
             return 0;
-        }
         int n = matrix[0].length;
         int[][] left = new int[m][n];
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    left[i][j] = (j == 0 ? 0 : left[i][j-1]) + 1;
-                }
+                if (matrix[i][j] == '1')
+                    left[i][j] = (j==0 ? 0: left[i][j-1])+1;
             }
         }
         int ret = 0;
+
         for (int j = 0; j < n; j++) {
+            Deque<Integer> stack = new LinkedList<>();
             int[] up = new int[m];
             int[] down = new int[m];
-            Deque<Integer> stack = new LinkedList<>();
             for (int i = 0; i < m; i++) {
-                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
+                if (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
                     stack.pop();
                 }
                 up[i] = stack.isEmpty() ? -1 : stack.peek();
@@ -31,16 +29,14 @@ public class Solution85 {
             }
             stack.clear();
             for (int i = m-1; i >= 0; i--) {
-                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
+                if (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
                     stack.pop();
                 }
                 down[i] = stack.isEmpty() ? m : stack.peek();
                 stack.push(i);
             }
             for (int i = 0; i < m; i++) {
-                int height = down[i] - up[i] - 1;
-                int area = height * left[i][j];
-                ret = Math.max(ret, area);
+                ret = Math.max(ret, (down[i]- up[i] - 1)*left[i][j]);
             }
         }
         return ret;
