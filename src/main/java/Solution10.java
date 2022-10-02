334410e7bb0edc7b89999e53a68841a413227208
user-lmz <mingzeluo888@gmail.com>
2022-10-02 20:28:29 +0800

    a

---
+++ src/main/java/Solution10.java
@@ -0,0 +1,42 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Solution10 {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] f = new boolean[m + 1][n + 1];
+
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (maches(s, p, i, j - 1)) {
+                        f[i][j] = f[i - 1][j];
+                    }
+                } else {
+                    if (maches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public boolean maches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(true, isMatch("aaa", "ab*a*c*a"));
+    }
+}
