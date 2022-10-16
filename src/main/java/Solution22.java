c4d0017931b01828777b9b3685212f29ff083cc8
user-lmz <mingzeluo888@gmail.com>
2022-10-16 10:26:44 +0800

    add Solution22

---
+++ src/main/java/Solution22.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import java.util.List;
+
+public class Solution22 {
+
+    public List<String> generateParenthesis(int n) {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
